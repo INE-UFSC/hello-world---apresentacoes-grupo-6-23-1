889ade47b4d256a2f17612e1b9890e374f7c487f
Provavelmente-o-Caio <caiofecardoso@hotmail.com>
2023-03-07 13:48:19 -0300

    introdução Caio

--- membro2.py
+++ membro2.py
@@ -1,2 +1,6 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o Caio Ferreira Cardoso, tenho 18 anos, sou de Imbituba em Santa Catarina, além do estudo da computação meus hobbies são jogos, leitura e estudo de história. Comecei minha experiência com programação na infância fazendo modificações em jogos, porém aumentei minha experiência durante o ensino médio enquanto fazia um ensino médio técnico (feito pelo IFSC) na área. Com esta disciplina espero aumentar ainda mais minhas habilidades com programação, principalmente criar projetos de maior escala, algo que evitei fazer até então por falta de confiança em minhas habilidades atuais."
+#escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+
+
+print(hello())
